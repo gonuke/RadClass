ea00da0730841a9f41e9524d585fdf4dd58fb3de
Jordan Stomps <stomps@wisc.edu>
2021-06-09 14:43:28 -0500

    cleaning algorithm and adding documentation

--- RadClass/BackgroundEstimator.py
+++ RadClass/BackgroundEstimator.py
@@ -4,13 +4,37 @@ import math
 
 
 class BackgroundEstimator:
-    background = pd.DataFrame(columns=['timestamp', 'count-rate'])
-    data = np.empty((0, 2))
-    # sample_size = None
+    '''
+    Integrates a gross count rate from a spectrum and orders samples in
+        ascending order of magnitude. Saves a certain percentage samples
+        to file with corresponding gross count rate.
 
-    def __init__(self, confidence=0.95, sample_size=None):
+    Attributes:
+    background: Pandas DataFrame used to store timestamps and count rates.
+    data: Numpy array used to incrementally store data throughout analysis
+        before sending/saving in background.
+    confidence: Percentage of samples to disregard. 1-confidence number of
+        samples are saved. For example, if confidence=0.95, the lowest 5% of
+        count rates/timestamps are saved.
+    ofilename: Output filename excluding extension. File is stored as a csv.
+        Default is bckg_results.
+    '''
+
+    background = pd.DataFrame(columns=['timestamp', 'count_rate'])
+    data = np.empty((0, 2))
+
+    def __init__(self, confidence=0.95, ofilename='bckg_results'):
         self.confidence = confidence
-        self.sample_size = sample_size
+        self.ofilename = ofilename
+
+    def __del__(self):
+        '''
+        Saves results upon completion of script, even if the script crashes.
+        '''
+
+        self.estimate()
+        # this method could be compressed to save memory
+        self.background.to_csv(self.ofilename+'.csv', index=False)
 
     def save_all(self, observations):
         data = pd.DataFrame()
@@ -19,32 +43,56 @@ class BackgroundEstimator:
             data[str(i+1)] = observations[:, i+1]
         data.to_csv('results/oct2019_channels.csv', mode='a')
 
-    def add_obs(self, timestamp, obs):
-        count_rate = np.sum(obs)
-        dfrow = {'timestamp': timestamp, 'count-rate': count_rate}
-        self.background.loc[len(self.background.index)] = dfrow
-
-        # using numpy for building instead of pandas
-        # self.data = np.vstack([self.data, [timestamp, count_rate]])
-
-        if self.sample_size is not None:
-            self.estimate()
-
     def sort(self):
-        return self.background.sort_values(by='count-rate')
+        '''
+        Wrapper function for Pandas DataFrame sort_values method. Organizes
+            samples in ascending order of gross count rate.
+        '''
+
+        return self.background.sort_values(by='count_rate', ascending=True)
 
     def estimate(self):
+        '''
+        Saves data to a Pandas DataFrame and selects a subset with the smallest
+            gross count rates.
+        
+        Attributes:
+        samples_size: The total number of samples analyzed.
+        num_samples: The number of samples to select and save.
+        '''
         # building pandas DataFrame once from numpy array
-        # self.background['timestamp'] = self.data[:,0]
-        # self.background['count-rate'] = self.data[:,1]
+        self.background['timestamp'] = self.data[:, 0]
+        self.background['count_rate'] = self.data[:, 1]
+
         # find number of background samples
-        if self.sample_size is None:
-            print('Not working :(')
-            total = len(self.background.index)
-        elif self.sample_size is not None:
-            total = self.sample_size
-        num_samples = math.floor(total * (1-self.confidence))
+        sample_size = len(self.background.index)
+        # +1 to account for indexing
+        num_samples = math.floor(sample_size * (1-self.confidence)) + 1
 
+        # sort and separate the smallest count-rates
         self.background = self.sort()
+        self.background = self.background.iloc[:num_samples]
 
-        return self.background.iloc[:num_samples]
+    def run(self, data, timestamp):
+        '''
+        Method called by parent class during analysis. Integrates/collects a
+            gross count rate and stores it in an iterim NumPy array.
+
+        Attributes:
+        count_rate: gross count rate from individual observation.
+        '''
+
+        count_rate = np.sum(data)
+
+        # alternative: use pandas and "append"
+        # slower (by 3 and 20 minutes respectively) than storing in array
+        # dfrow = {'timestamp': timestamp, 'count-rate': count_rate}
+        # self.background.loc[len(self.background.index)] = dfrow
+        # self.background = pd.concat([self.background, pd.DataFrame([data], index=[timestamp])])
+
+        # using numpy to build lightweight matrix instead of pandas
+        self.data = np.vstack([self.data, [timestamp, count_rate]])
+
+        # instead of estimating at the end, checkpointing could occur
+        # if len(self.data) % 10000:
+        #    self.estimate()
